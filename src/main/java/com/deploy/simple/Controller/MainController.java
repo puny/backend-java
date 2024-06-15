f4413ede4d5892a29757ce6a2af3cff9d7a654fb
puny80 <puny80@chollian.net>
2024-06-15 10:30:48 +0900

    add index page

---
+++ src/main/java/com/deploy/simple/Controller/MainController.java
@@ -0,0 +1,17 @@
+package com.deploy.simple.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class MainController {
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String requestIndex() {
+        return "hello spring";
+    }
+    
+}
